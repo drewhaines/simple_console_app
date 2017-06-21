082b6a234fb726bbfb447cb6b924458b2909cccb
Drew Haines <drewhaines@Drews-MacBook-Pro.local>
2017-06-21 13:46:43 -0700

    set up a console project

---
+++ drews_console_app/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace drews_console_app
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!!!");
+        }
+    }
+}
